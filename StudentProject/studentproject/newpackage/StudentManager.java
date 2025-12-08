9d6729ee9ef9e326e7713b5a42cec746c76dcb18
gadeer-mm <gadeer2003mm@gmail.com>
2025-12-08 16:46:33 +0300

    Update StudentManager.java
    Update StudentManager.java to implement IStudentManager interface

--- StudentProject/studentproject/newpackage/StudentManager.java
+++ StudentProject/studentproject/newpackage/StudentManager.java
@@ -1,16 +1,14 @@
 package studentproject.newpackage;
 
-
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class StudentManager {
+public class StudentManager implements IStudentManager {  
 
     private ArrayList<Student> students = new ArrayList<>();
 
-
+    @Override
     public void addStudent(Student s) {
         for (Student existingStudent : students) {
             if (existingStudent.getId().equals(s.getId())) {
@@ -18,11 +16,10 @@ public class StudentManager {
                 return;  
             }
         }
-        
         students.add(s);
     }
 
-    
+    @Override
     public Student search(String id) {
         for (Student s : students) {
             if (s.getId().equals(id)) {
@@ -32,7 +29,7 @@ public class StudentManager {
         return null; 
     }
 
-   
+    @Override
     public boolean updateStudent(String id, String newName, double newGpa, String newDept, int newYear) {
         Student s = search(id);
         if (s != null) {
@@ -45,7 +42,7 @@ public class StudentManager {
         return false;  
     }
 
-   
+    @Override
     public boolean deleteStudent(String id) {
         Student s = search(id);
         if (s != null) {
@@ -55,7 +52,7 @@ public class StudentManager {
         return false;  
     }
 
-    
+    @Override
     public void saveToFile() throws IOException {
         FileWriter fw = new FileWriter("students.txt");
         for (Student s : students) {
@@ -64,7 +61,7 @@ public class StudentManager {
         fw.close();  
     }
 
-   
+    @Override
     public void loadFromFile() throws IOException {
         students.clear();  
 
@@ -87,19 +84,21 @@ public class StudentManager {
         sc.close();  
     }
 
-    
+    @Override
     public void printAll() {
         for (Student s : students) {
             System.out.println(s);  
         }
     }
-    
+
+    @Override
     public void reportByDepartment(String dept) {
-        dept=dept.trim();
+        dept = dept.trim();
         for (Student s : students) {
             if (s.getDepartment().equalsIgnoreCase(dept)) {
                 System.out.println(s);
             }
         }
     }
+}
 }
